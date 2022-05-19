da15d5e40490b8cdfa9aa32a5054eccb6314324a
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-19 04:06:52 +0300

    Add files via upload

---
+++ array_functions.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php
+	
+	// All functions :https://www.php.net/manual/en/ref.array
+
+	$list = [4,5,6,7,8];
+
+	echo max($list) . "</br>";
+	echo min($list) . "</br>";
+
+	print_r($list);
+
+	sort($list);
+
+	print_r($list);
+	
+	?>
+
+
+</body>
+</html>
---
+++ math_functions.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php
+
+	//All functions : https://www.php.net/manual/en/ref.math.php
+
+	
+	echo pow(2,6); //64
+
+	echo "</br>";
+
+	echo rand(1, 1000); // Random number from 1 to a 1000
+	
+	echo "</br>";
+
+	echo sqrt(100); //Takes square root from number
+
+	echo "</br>";
+
+	echo ceil(4.5); //Round to upper num (5)
+
+	echo "</br>";
+
+	echo floor(4.5); //Round to down num (4)
+	?>
+
+
+</body>
+</html>
