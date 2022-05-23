c428820f8b9ae7043b1ed27a250d815cc007a523
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-23 15:54:23 +0300

    Add files via upload

---
+++ mysql/functions.php
@@ -0,0 +1,54 @@
+<?php
+include "db.php";
+
+function createRows(){
+	global $connection;
+	if(isset($_POST['submit'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	
+		$query = "INSERT INTO users(username,password) ";
+		$query .= "VALUES ('$username', '$password')";
+	
+		$result = mysqli_query($connection, $query);
+		if(!$result) {
+			die('Query FAILED' . mysqli_error());
+		} else
+			echo "Record created";
+	}
+}
+
+
+function updateRows(){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$id = $_POST['id'];
+
+		$query = "UPDATE users 
+		SET username = '$username', password = '$password'
+		WHERE id = $id";
+
+		$result = mysqli_query($connection, $query);
+
+		if (!$result) {
+			die ("Query error");
+		} else
+		echo "Record updated";
+}
+}
+
+function deleteRows(){
+	global $connection;
+	$id = $_POST['id'];
+
+	$query = "DELETE FROM users ";
+	$query .= "WHERE id = $id ";
+
+	$result = mysqli_query($connection, $query);
+	if (!$result) {
+		die ("Query error" . mysqli_error($connection));
+} else
+echo "Record deleted";
+}
+}
+?>
