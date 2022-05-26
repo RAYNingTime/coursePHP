eccb621d91d0f941762830fd44b2fb2f4472ebcb
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-26 15:01:06 +0300

    Add files via upload

---
+++ get.php
@@ -0,0 +1,31 @@
+<?php
+
+print_r($_GET);
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+<?php
+	$id = 100;
+
+	echo "<br>";
+
+	if (!$_GET)
+		echo "<a href='get.php?id=" . $id . "'>SEND PARAMENTER</a>";
+?>
+
+
+
+
+
+</body>
+</html>
