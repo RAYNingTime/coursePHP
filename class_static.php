23c3f940827c2f1742077a4c74b456766d3f9524
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-27 13:14:33 +0300

    Add files via upload

---
+++ class_static.php
@@ -0,0 +1,26 @@
+<?php
+
+class Car{
+
+	static $wheels = 4;
+	var $hood = TRUE;
+
+	//Static could be a function as well
+	public static function addWheels(){
+		Car::$wheels += 2;
+	}
+
+}
+
+$bmw = new Car();
+
+//Way to use static varibles
+echo Car::$wheels . "<br>";
+
+Car::addWheels();
+
+//Way to use static varibles
+echo Car::$wheels . "<br>";
+
+
+?>
