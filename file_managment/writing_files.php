820be6c3270d2e1e3f3d663949e1178ac07dc702
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-05-27 13:15:32 +0300

    Add files via upload

---
+++ file_managment/writing_files.php
@@ -0,0 +1,15 @@
+<?php
+$file = "example.txt";
+
+//Opens a file to 'w' - write something in it
+if($handle = fopen($file,'w')) {
+
+	fwrite($handle, "Lmao, this is some text");
+
+	fclose($handle);
+} else
+echo "The application was not able to write on the file";
+
+
+
+?>
