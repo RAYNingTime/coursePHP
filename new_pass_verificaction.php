16167be90400e40070279f3bf93d1937fb579f2c
Ivan (John) Kosiakov <92152254+RAYNingTime@users.noreply.github.com>
2022-06-10 14:03:07 +0300

    Add files via upload

---
+++ new_pass_verificaction.php
@@ -0,0 +1,17 @@
+<?php
+
+
+	$password_from_DB = password_hash('password', PASSWORD_DEFAULT, array('cost' => 10));
+	
+	//Password from Data Base
+	$password = 'password';
+
+	if (password_verify($password, $password_from_DB))
+		echo "<h1>Welcome!!</h1>";
+
+	//So there is no need to somehow implement pass from user, you just need to use function.
+
+
+
+
+?>
